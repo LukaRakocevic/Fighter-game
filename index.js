6a00a823a873631525f0338c701ab50626de1e44
Luka Rakocevic <95525958+LukaRakocevic@users.noreply.github.com>
2022-04-21 22:35:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,19 +9,25 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.8
 //constructor
 class Sprite {
-    constructor({position, velocity, color = 'red'}){
+    constructor({position, velocity, color = 'red', offset}){
  this.position = position
  this.velocity = velocity
+ this.width = 50
  this.height = 150
  this.lastKey
  this.attackBox = {
-     position:this.position ,
+     position:{
+         x:this.position.x,
+         y:this.position.y
+     } ,
+     offset,
      width: 100,
      height: 50
+    
  }
 
  this.color = color
-
+    this.isAttacking = false
     }
 
 
@@ -30,16 +36,24 @@ draw(){
  //player
     c.fillStyle = this.color
 
-    c.fillRect(this.position.x, this.position.y, 50, this.height)
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
   
  //attackBox
+ if (this.isAttacking) {
  c.fillStyle = 'green'
-    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+    c.fillRect(this.attackBox.position.x, 
+        this.attackBox.position.y, 
+        this.attackBox.width, 
+        this.attackBox.height)
+    }
 }
 
 update(){
     this.draw()
  
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 // loop koji omogucava modelu da ne propadne kroz patos
@@ -48,31 +62,46 @@ update(){
         this.velocity.y = 0;
         } else this.velocity.y += gravity; // GRAVITACIJA
     }
+    attack(){
+        this.isAttacking = true
+        setTimeout (() => {
+            this.isAttacking  = false
+        }, 100)
+    }
 }
 
 
 const player = new Sprite ({
     position: {
-x:0,
-y:0
-},
-velocity: {
-x:0,
-y:10
-},
+        x:0,
+        y:0
+    },
+    velocity: {
+        x:0,
+        y:10
+    },
+    offset: {
+        x:0,
+        y:0
+    },
 
-color: 'blue'
+
 })
 
 const enemy = new Sprite ({
     position: {
-x:400,
-y:100
-},
+        x:400,
+        y:100
+    },
     velocity: {
-x:0,
-y:0
-},
+        x:0,
+        y:0
+    },
+    color: 'blue',
+    offset: {
+        x:-50,
+        y:0
+    },
 
 })
 
@@ -98,13 +127,18 @@ ArrowUp:{
     pressed:false
 }
 }
+function rectangularCollision ({ rectangle1, rectangle2}){
+    return(
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+};
 
 function animate (){
 
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    console.log('go')
+    //console.log("go");
     player.update()
     enemy.update()
 
@@ -125,10 +159,25 @@ function animate (){
         else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
             enemy.velocity.x = 8 //ne zaboraviti enemy.lastKey!!!!!
         }
-        //collision detect
 
-        if(player.attackBox.position.x + player.attackBox.width >= enemy.position.x) {
-            console.log("JEEEESI JESI, VAZI STO SI ZNAO DA SAM TU!!!!!!");
+        //collision detect
+        if(rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) && 
+            player.isAttacking
+            ){
+            player.isAttacking = false
+            document.querySelector('#enemyHealth').style.width = '10%'
+        }
+
+        if(rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) && 
+            enemy.isAttacking) {
+            enemy.isAttacking = false
+            console.log("enemy attacks");
         }
 }
 
@@ -141,41 +190,47 @@ animate()
 // "d" nalaze player-u da ide desno, "a" levo, "f" je attack itd
 window.addEventListener('keydown', (event)=>{
     switch (event.key){
-case 'd':
-    keys.d.pressed = true;
-    player.lastKey = 'd';
-break
+        case 'd':
+            keys.d.pressed = true;
+            player.lastKey = 'd';
+        break
 
-case 'a':
-    keys.a.pressed = true;
-    player.lastKey = 'a';
-break   
+        case 'a':
+            keys.a.pressed = true;
+            player.lastKey = 'a';
+        break   
 
-case 'w':
-    player.velocity.y = -16 //skakanje
-    player.lastKey = 'w';
-break   
-
-
-//enemy
-
-case 'ArrowLeft':
-    keys.ArrowLeft.pressed = true;
-    enemy.lastKey = 'ArrowLeft';
-break
-
-case 'ArrowRight':
-    keys.ArrowRight.pressed = true;
-    enemy.lastKey = 'ArrowRight';
-break   
-
-case 'ArrowUp':
-    enemy.velocity.y = -16 //skakanje
-    enemy.lastKey = 'ArrowUp';
-break   
+        case 'w':
+            player.velocity.y = -16 //skakanje
+            player.lastKey = 'w';
+        break   
+        case ' ':
+            player.attack();
+            break;
 
 
-}
+        //enemy
+
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = 'ArrowLeft';
+        break;
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = 'ArrowRight';
+        break; 
+
+        case 'ArrowUp':
+            enemy.velocity.y = -16 //skakanje
+            enemy.lastKey = 'ArrowUp';
+        break;  
+
+        case 'Control':
+            enemy.attack();
+        break;
+
+        }
     console.log(event);
 })
 
@@ -183,29 +238,29 @@ break
 // "d" bi isao zauvek desno
 window.addEventListener('keyup', (event)=>{
     switch (event.key){
-    case 'd':
-        keys.d.pressed = false;; //pustanjem "d" prestaje da se krece
-    break
+        case 'd':
+            keys.d.pressed = false;; //pustanjem "d" prestaje da se krece
+        break
 
-    case 'a':
-        keys.a.pressed = false;
-    break
+        case 'a':
+            keys.a.pressed = false;
+        break
 
-    case 'w':
-        keys.a.pressed = false;
-    break //do I need this??????
+        case 'w':
+            keys.a.pressed = false;
+        break //do I need this??????
 
 
-    case 'ArrowRight':
-        keys.ArrowRight.pressed = false;; //pustanjem "d" prestaje da se krece
-    break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;; //pustanjem "d" prestaje da se krece
+        break
 
-    case 'ArrowLeft':
-        keys.ArrowLeft.pressed = false;
-    break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+        break
     }
-        console.log(event);
-    })
+    console.log(event);
+})
 
 
 
