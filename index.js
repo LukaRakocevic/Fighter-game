e623fbb92f8cb8bc10ccd18224175aea5513dd4b
Luka Rakocevic <dr.dexxter@gmail.com>
2022-04-26 01:08:15 +0200

    react to strikes, receive hit, death animation done

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const player = new Fighter ({
     },
     velocity: {
         x:0,
-        y:10
+        y:0
     },
     offset: {
         x:0,
@@ -65,6 +65,22 @@ const player = new Fighter ({
             imageSrc: './oak_woods_v1.0/samuraiMack/Attack1.png',
             framesMax: 6,
         },
+        takeHit:{
+            imageSrc: './oak_woods_v1.0/samuraiMack/Take Hit.png',
+            framesMax: 4,
+        },
+        death:{
+            imageSrc: './oak_woods_v1.0/samuraiMack/Death.png',
+            framesMax: 6,
+        },
+    },
+    attackBox: {
+        offset:{
+            x:100,
+            y:50
+        },
+        width: 150,
+        height: 50
     }
 }
     
@@ -87,7 +103,9 @@ const enemy = new Fighter ({
     imageSrc: './oak_woods_v1.0/kenji/idle.png',
     framesMax: 4,
     scale: 2.5,
-    offset:{x:215, y:157
+    offset:{
+        x:215, 
+        y:169
     },
     sprites:{
         idle:{
@@ -110,6 +128,22 @@ const enemy = new Fighter ({
             imageSrc: './oak_woods_v1.0/kenji/Attack1.png',
             framesMax: 4,
         },
+        takeHit: {
+            imageSrc: './oak_woods_v1.0/kenji/Take hit.png',
+            framesMax: 3,
+        },
+        death: {
+            imageSrc: './oak_woods_v1.0/kenji/Death.png',
+            framesMax: 7,
+        },
+    },
+    attackBox: {
+        offset:{
+            x:-170,
+            y:50
+        },
+        width: 170,
+        height: 50
     }
 
 })
@@ -138,7 +172,7 @@ ArrowUp:{
 }
 
 
-    decreaseTimer();
+decreaseTimer();
 
 
 function animate (){
@@ -149,15 +183,16 @@ function animate (){
     background.update()
     shop.update()
     player.update()
-  
+    enemy.update()
     //vuce iz const keys objekte, bitno za smoother left-right akciju
     //da bi se izbegao problem kao sto je u mugen-u
     player.velocity.x = 0;//bez ovoga player bi isao zauvek u jednom smeru, ovo je default value za velocity
     enemy.velocity.x = 0;
 
     
+
+    //<======= PLAYER movement
     if (keys.a.pressed && player.lastKey === 'a'){    
-         //<======= PLAYER movement
         player.velocity.x = -8
         player.switchSprite('run')
     }
@@ -177,35 +212,66 @@ function animate (){
         player.switchSprite('fall')
     }
 //....................
+
+
+//<======= ENEMY movement
         if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){    
-         //<======= ENEMY movement
             enemy.velocity.x = -8
+            enemy.switchSprite('run')
         }
         else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
-            enemy.velocity.x = 8 //ne zaboraviti enemy.lastKey!!!!!
+            enemy.velocity.x = 8
+            enemy.switchSprite('run')
+        } else {
+            enemy.switchSprite('idle')
         }
+    //jumping
+    if(enemy.velocity.y <0){
+        enemy.switchSprite('jump')
+    }
+    //player.velocity.y >0 means falling!
+    else if (enemy.velocity.y >0){
+        enemy.switchSprite('fall')
+    } 
 
-        //collision detect
+        
+        //collision detect & enemy gets hit
         if(rectangularCollision({
             rectangle1: player,
             rectangle2: enemy
         }) && 
-            player.isAttacking
+            player.isAttacking && 
+            player.framesCurrent === 4
             ){
-            player.isAttacking = false
-            enemy.health -= 10
-            document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+                enemy.takeHit()
+             player.isAttacking = false
+             
+             document.querySelector('#enemyHealth').style.width = enemy.health + '%'
         }
 
+        //if player misses
+        if (player.isAttacking && player.framesCurrent ===4) {
+            player.isAttacking = false
+        }
+
+        //player gets hit
         if(rectangularCollision({
             rectangle1: enemy,
             rectangle2: player
         }) && 
-            enemy.isAttacking) {
+            enemy.isAttacking && 
+            enemy.framesCurrent === 2
+            ) {
+            player.takeHit()
             enemy.isAttacking = false
-            player.health -= 10
-            document.querySelector('#playerHealth').style.width = player.health + '%'
+                        document.querySelector('#playerHealth').style.width = player.health + '%'
         }
+
+                //if player? misses
+                if (enemy.isAttacking && enemy.framesCurrent ===2) {
+                   
+                    enemy.isAttacking = false
+                }
 
         //end game based on health
         if (enemy.health <= 0 || player.health <=0){
@@ -221,28 +287,31 @@ animate()
 //i definise sta se desava sa razlocitim key presovima,
 // "d" nalaze player-u da ide desno, "a" levo, "f" je attack itd
 window.addEventListener('keydown', (event)=>{
-    switch (event.key){
-        case 'd':
+
+      if (!player.dead) {        
+        
+          switch (event.key){
+          case 'd':
             keys.d.pressed = true;
             player.lastKey = 'd';
-        break
+           break
 
-        case 'a':
+          case 'a':
             keys.a.pressed = true;
             player.lastKey = 'a';
-        break   
+          break   
 
-        case 'w':
+               case 'w':
             player.velocity.y = -16 //skakanje
             player.lastKey = 'w';
-        break   
-        case ' ':
+               break   
+            case ' ':
             player.attack();
             break;
-
-
-        //enemy
-
+      }
+    }
+        if (!enemy.dead){
+       switch(event.key){
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = true;
             enemy.lastKey = 'ArrowLeft';
@@ -261,10 +330,9 @@ window.addEventListener('keydown', (event)=>{
         case 'Control':
             enemy.attack();
         break;
-
+            }
         }
-    console.log(event);
-})
+    })
 
 //deo koji PREKIDA pomeranje playera, bez ovoga pritiskom na 
 // "d" bi isao zauvek desno
