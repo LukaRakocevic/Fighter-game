d90226d0c87f89886a74a4a54d9296853be8de7b
Luka Rakocevic <dr.dexxter@gmail.com>
2022-04-26 02:26:36 +0200

    finished

--- index.js
+++ index.js
@@ -182,6 +182,8 @@ function animate (){
     c.fillRect(0, 0, canvas.width, canvas.height)
     background.update()
     shop.update()
+    c.fillStyle = 'rgba(255, 255, 255, 0.1)'
+    c.fillRect(0,0,canvas.width, canvas.height)
     player.update()
     enemy.update()
     //vuce iz const keys objekte, bitno za smoother left-right akciju
@@ -246,7 +248,9 @@ function animate (){
                 enemy.takeHit()
              player.isAttacking = false
              
-             document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+                          gsap.to('#enemyHealth', {
+                 width: enemy.health + '%'
+             })
         }
 
         //if player misses
@@ -264,7 +268,10 @@ function animate (){
             ) {
             player.takeHit()
             enemy.isAttacking = false
-                        document.querySelector('#playerHealth').style.width = player.health + '%'
+
+            gsap.to('#playerHealth', {
+                width: player.health + '%'
+            })
         }
 
                 //if player? misses
